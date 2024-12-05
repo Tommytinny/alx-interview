d754b8936394cc0978796bea4f5da6962f2dbced
Tommytinny <tommyteenboi@gmail.com>
2024-12-05 10:36:35 +0100

    fix errors in code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,7 +7,7 @@ def island_perimeter(grid):
     """Computes the perimeter of an island with no lakes.
     """
     perimeter = 0
-    if isinstance(grid) != list:
+    if type(grid) != list:
         return 0
     n = len(grid)
     for i, row in enumerate(grid):
